3065ce40f974584cfc5d0063753b1ad5a8f601d2
Anil Bommidi <abommidi@vivifyhealthcare.com>
2023-02-22 16:13:03 +0530

    openhealthbot api's

--- openhealth/openhealth/settings.py
+++ openhealth/openhealth/settings.py
@@ -53,6 +53,16 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.AllowAny',),
+
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+
+    ],
+
+}
+
 
 
 SWAGGER_SETTINGS = {
--- openhealth/openhealth/urls.py
+++ openhealth/openhealth/urls.py
@@ -6,12 +6,20 @@ from django.conf.urls import *
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 from rest_framework import permissions
+from rest_framework_simplejwt import views as jwt_views
+from django.utils.crypto import get_random_string
 
 
 
 
 
+def token():
+    unique_id = get_random_string(length=32)
+    return unique_id
 
+
+token = token()
+support_id = ""
 
 
 ######## Version #########
@@ -26,6 +34,12 @@ schema_view = get_schema_view(
 )
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('Api/token/',
+         jwt_views.TokenObtainPairView.as_view(),
+         name='token_obtain_pair'),
+    path('Api/token/refresh/',
+         jwt_views.TokenRefreshView.as_view(),
+         name='token_refresh'),
     path('OpenHealthBot/', include('openhealthapi.urls')),
     path('swagger/' , schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     
